688e65ad784c7a84491025499398eea946fd6b26
Beata Huszar <beata.huszar@playngo.com>
2022-03-10 14:49:25 +0100

    feat: add routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,9 +49,9 @@ export default class Directory extends React.Component {
 		return (
 			<div className="directory-menu">
 				{
-					this.state.sections.map(({ id, title, imageUrl, size }) => {
+					this.state.sections.map(({ id, ...otherSectionProps }) => {
 						return (
-							<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+							<MenuItem key={id} {...otherSectionProps}/>
 						)
 					})
 				}
