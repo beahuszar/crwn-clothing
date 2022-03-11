67cc9e48dc8cec1b72ea1a218625262e813dcfba
Beata Huszar <beata.huszar@playngo.com>
2022-03-11 16:04:12 +0100

    feat: handle user auth state with firebase

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/shop" component={ShopPage}></Route>
-        <Route exact path="/signin" component={SignInAndSignUpPage}></Route>
-      </Switch>
-    </div>
-  );
-}
-
-export default App;
+export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    }
+  }
+  
+  unsubscribeFromAuth = null;
+  
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+    })
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/signin" component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
+};
