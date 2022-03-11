6bfdd578c3414a2a249441d573f3e685acfa8594
Beata Huszar <beata.huszar@playngo.com>
2022-03-11 19:29:42 +0100

    fix: add button type to googleSignIn btn as it is handled as submit within the form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,7 @@ export default class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
           </div>
         </form>
       </div>
