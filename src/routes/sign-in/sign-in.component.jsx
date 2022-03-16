fd9f5e6f6a215037af1aba05aa2d09a840c9f8c8
Beata Huszar <beata.huszar@playngo.com>
2022-03-16 18:27:08 +0100

    feat: add authentication and save new users in db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,15 @@
-import {signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
+import {createUserDocumentFromAuth, signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response)
+  const saveGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
   };
   
   return (
     <div>
       <h1>signinpage</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={saveGoogleUser}>Sign in with Google Popup</button>
     </div>
   )
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnGK6HKhft17PFLHuGjpcBJnABYL27pZc",
@@ -21,3 +22,27 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+    
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt
+      });
+    } catch (e) {
+      console.error("error creating the user ", e.message);
+    }
+  }
+  
+  return userDocRef;
+};
