7381072b0a36d298ae1159d916657f987fca3f4d
Beata Huszar <beata.huszar@playngo.com>
2022-03-16 16:31:30 +0100

    feat: use OUTLET in routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Home from "./routes/home/home.component";
-import {Route, Routes} from "react-router-dom";
+import {Outlet, Route, Routes} from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+};
 
 const App = () => (
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />} />
+    </Route>
   </Routes>
 );
 
