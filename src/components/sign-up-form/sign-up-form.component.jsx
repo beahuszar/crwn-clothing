d7754ddc418805dc83b0550feeb42d2f99bc5bf2
Beata Huszar <beata.huszar@playngo.com>
2022-03-17 10:39:31 +0100

    feat: add sign-up with email functionality

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
 
 const defaultFormfields = {
   displayName: "",
@@ -11,16 +12,40 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormfields);
   const { displayName, email, password, confirmPassword } = formFields;
   
+  const resetFormFields = () => {
+    setFormFields(defaultFormfields);
+  };
+  
   const handleChange = (event) => {
     const { name, value } = event.target;
     
     setFormFields({ ...formFields, [name]: value});
   };
   
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords are not matching");
+      return;
+    }
+    
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password);
+      const userDocRef = await createUserDocumentFromAuth(user, {displayName});
+      resetFormFields();
+    } catch (e) {
+      if (e.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+      } else {
+        console.error("user creation encountered and error: ", e.message);
+      }
+    }
+  };
+  
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
   
