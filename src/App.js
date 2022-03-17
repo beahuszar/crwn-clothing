f1773367a159958e93a233609c077b136ed4698f
Beata Huszar <beata.huszar@playngo.com>
2022-03-17 18:20:54 +0100

    feat: add signin with email and pwd option

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 import {Route, Routes} from "react-router-dom";
-import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigation />}>
       <Route index element={<Home />} />
       <Route path="shop" element={<div>I am the shop</div>} />
-      <Route path="sign-in" element={<SignIn />} />
+      <Route path="auth" element={<Authentication />} />
     </Route>
   </Routes>
 );
