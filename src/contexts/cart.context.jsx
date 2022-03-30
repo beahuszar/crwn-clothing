5542330154b430fc18fc43a40a69f320ded56ac8
beahuszar <husz.beata@gmail.com>
2022-03-30 18:58:08 +0200

    feat: add add-to-cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,34 @@
 import {createContext, useState} from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  const existingItem = cartItems.find(item => item.id === productToAdd.id);
+  
+  if (existingItem) {
+    return cartItems.map(item => (item.id === productToAdd.id ?
+        {...item, quantity: item.quantity + 1} :
+        item
+    ))
+  }
+  
+  return [...cartItems, {...productToAdd, quantity: 1}];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
-  setOpen: () => {}
+  setOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {}
 });
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const providerProps = {isCartOpen, setIsCartOpen};
+  const [cartItems, setCartItems] = useState([]);
+  
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  
+  const providerProps = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
   
   return <CartContext.Provider value={providerProps}>{children}</CartContext.Provider>
 };
