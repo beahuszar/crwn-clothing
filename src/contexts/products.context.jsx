19f5429c43fac88671194ddb6645f6d530c150fa
beahuszar <husz.beata@gmail.com>
2022-04-01 17:38:25 +0200

    feat: fetching categories from the database

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,4 +1,5 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
+import {getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
 
 
 export const ProductsContext = createContext({
@@ -8,6 +9,20 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({children}) => {
   const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    /**
+     * useEffect's callback cannot be async,
+     * but a separate async function
+     * has to be made inside of ot
+     * */
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    
+    getCategoriesMap();
+  }, []);
   const contextProps = {products, setProducts};
   
   return <ProductsContext.Provider value={contextProps}>{children}</ProductsContext.Provider>
