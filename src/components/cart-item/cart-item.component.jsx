513cd7628c22e7034516266b0fcd1070a4d50f6e
beahuszar <husz.beata@gmail.com>
2022-04-06 15:37:50 +0200

    feat: cart-item component to styled

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,15 @@
-import "./cart-item.styles.scss";
+import {CartItemContainer, Img, ItemDetails, Name} from "./cart-item.styles";
 
 const CartItem = ({ cartItem }) => {
   const { name, quantity, price, imageUrl } = cartItem;
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={name}/>
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price">{quantity} × ${price}</span>
-      </div>
-    </div>
+    <CartItemContainer>
+      <Img src={imageUrl} alt={name}/>
+      <ItemDetails>
+        <Name>{name}</Name>
+        <span>{quantity} × ${price}</span>
+      </ItemDetails>
+    </CartItemContainer>
   )
 };
 
