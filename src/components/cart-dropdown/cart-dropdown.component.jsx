b1e0df99e4138c3f333113cd9a9482b49f45f608
beahuszar <husz.beata@gmail.com>
2022-04-06 15:16:56 +0200

    feat: cart dropdown to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,9 @@
-import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
 import {useNavigate} from "react-router-dom";
+import {CartDropdownContainer, CartItems} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const {cartItems} = useContext(CartContext);
@@ -14,12 +14,12 @@ const CartDropdown = () => {
   };
   
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.map(item => <CartItem key={item.id} cartItem={item} /> )}
-      </div>
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   )
 };
 
