de42b5d9e50896ab6791016e4ef62fe0bbb3c726
beahuszar <husz.beata@gmail.com>
2022-04-06 16:00:38 +0200

    feat: checkout-item component to styled

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,6 @@
-import "./checkout-item.styles.scss";
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
+import {CheckoutItemContainer, Img, Arrow, ImgContainer, QuantityContainer, ItemDetail, RemoveButton, Value} from "./checkout-item.styles";
 
 const CheckoutItem = ({cartItem}) => {
   const {name, imageUrl, price, quantity} = cartItem;
@@ -13,23 +13,23 @@ const CheckoutItem = ({cartItem}) => {
   const clearHandler = () => clearItemFromCart(cartItem);
   
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
-        <img src={imageUrl} alt={name}/>
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={removeHandler}>
+    <CheckoutItemContainer>
+      <ImgContainer>
+        <Img src={imageUrl} alt={name}/>
+      </ImgContainer>
+      <ItemDetail>{name}</ItemDetail>
+      <QuantityContainer>
+        <Arrow onClick={removeHandler}>
           &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={addHandler}>
+        </Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={addHandler}>
           &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={clearHandler}>&#10005;</div>
-    </div>
+        </Arrow>
+      </QuantityContainer>
+      <ItemDetail>{price}</ItemDetail>
+      <RemoveButton onClick={clearHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   )
 };
 
