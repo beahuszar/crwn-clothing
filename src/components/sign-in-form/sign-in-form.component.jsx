1fad4cdac20e1e24b98c612da69c6bbfe1f23557
beahuszar <husz.beata@gmail.com>
2022-04-06 18:19:28 +0200

    feat: sign-in-form component to styled

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,7 +2,7 @@ import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import {signInAuthUserWithEmailAndPassword, signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
 import {useState} from "react";
-import "./sign-in-form.styles.scss";
+import {SignInContainer, ButtonsContainer} from "./sign-in-form.styles";
 
 const defaultFormFields = {
   email: "",
@@ -46,18 +46,18 @@ const SignInForm = () => {
   };
   
   return (
-    <div className="sign-in-container">
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
         <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
-        <div className="buttons-container">
+        <ButtonsContainer>
           <Button type="submit">Sign in</Button>
           <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignInContainer>
   )
 };
 
