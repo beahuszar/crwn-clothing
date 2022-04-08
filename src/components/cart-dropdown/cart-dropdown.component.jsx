af1a9e07c4e94577fa909ad044943d450bb94c1c
beahuszar <husz.beata@gmail.com>
2022-04-08 18:09:42 +0200

    feat: targeting styled components in other components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import CartItem from "../cart-item/cart-item.component";
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context";
 import {useNavigate} from "react-router-dom";
-import {CartDropdownContainer, CartItems} from "./cart-dropdown.styles";
+import {CartDropdownContainer, CartItems, EmptyMessage} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const {cartItems} = useContext(CartContext);
@@ -16,7 +16,12 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.map(item => <CartItem key={item.id} cartItem={item} /> )}
+        {
+          cartItems.length ?
+            (cartItems.map(item => <CartItem key={item.id} cartItem={item} /> ))
+            :
+            <EmptyMessage>Your cart is empty</EmptyMessage>
+        }
       </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {BaseButton, GoogleSignInButton, InvertedButton} from "../button/button.styles";
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
@@ -12,6 +13,17 @@ export const CartDropdownContainer = styled.div`
   right: 40px;
   z-index: 5;
   display: flex;
+  
+  ${BaseButton},
+  ${GoogleSignInButton},
+  ${InvertedButton} {
+    margin-top: auto;
+  }
+`;
+
+export const EmptyMessage = styled.span`
+  font-size: 18px;
+  margin: 50px;
 `;
 
 export const CartItems = styled.div`
