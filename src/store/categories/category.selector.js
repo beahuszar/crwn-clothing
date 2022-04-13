196fb2eed54149da249ebbb9ffe89c110e44e61e
beahuszar <husz.beata@gmail.com>
2022-04-13 21:59:37 +0200

    feat: add reselect to category selector to memoize categoriesMap

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,5 +1,23 @@
-export const selectCategoriesMap = (state) => state.categories.categories.reduce((acc, category) => {
-  const {title, items} = category;
-  acc[title.toLowerCase()] = items;
-  return acc;
-}, {});
+import {createSelector} from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+/**
+ * The combiner will only run, if the state's categories property changes
+ * and that only happens, if an action related to that is fired
+ * otherwise it returns the memoized return value
+ * */
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const {title, items} = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
