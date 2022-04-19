9bae4270cc4da50c9c8f34639420ecbbbac80bb3
beahuszar <husz.beata@gmail.com>
2022-04-19 16:35:43 +0200

    feat: add persistance to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,23 @@
 import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./root-reducer";
 import logger from "redux-logger";
-
+import storage from "redux-persist/lib/storage"
+import {persistReducer, persistStore} from "redux-persist";
 /**
  * run before the action hits the reducer when it has been dispatched
  * */
 const middleWares = [logger];
 
+const persistConfig = {
+  key: "root",
+  storage, // localStorage
+  blacklist: ['user'] // dont persist user reducer, because it is coming from our auth state listener
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
