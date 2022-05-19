0a75532eff45464c0f6d14e3133b1641e16a371f
beahuszar <husz.beata@gmail.com>
2022-05-19 12:03:31 +0200

    refact: typing the custom middleware

--- src/store/middleware/logger.ts
+++ src/store/middleware/logger.ts
@@ -1,14 +1,17 @@
-export const loggerMiddleWare = (store) => (next) => (action) => {
+import {Middleware} from "redux";
+import {RootState} from "../store";
+
+export const loggerMiddleWare: Middleware<{}, RootState> = (store) => (next) => (action) => {
   if (!action.type)
   {
     return next(action);
   }
-  
+
   console.log(`type: ${action.type}`);
   console.log(`payload: ${action.payload}`);
   console.log(`currentState: ${store.getState()}`);
-  
+
   next(action);
-  
+
   console.log(`next state: ${store.getState()}`);
 };
