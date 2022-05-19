73ff853b85b6a681ee9b779f2c81ead5998e8602
beahuszar <husz.beata@gmail.com>
2022-05-19 12:08:19 +0200

    fix: wrong imports

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,9 @@
-import {Routes, Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
-import {FetchCategoriesStart} from '../../store/categories/category.action';
+import {fetchCategoriesStart} from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
--- src/store/categories/category.saga.js
+++ src/store/categories/category.saga.js
@@ -1,7 +1,7 @@
-import {takeLatest, call, put, all} from "redux-saga/effects"
+import {all, call, put, takeLatest} from "redux-saga/effects"
 import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
-import {FetchCategoriesFailure, FetchCategoriesSuccess} from "./category.action";
 import {CATEGORIES_ACTION_TYPES} from "./category.types";
+import {fetchCategoriesFailure, fetchCategoriesSuccess} from "./category.action";
 
 export function* fetchCategoriesAsync() {
   try {
