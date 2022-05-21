1e35d1329ed5bdd7da696b22f6def0c395c50013
beahuszar <husz.beata@gmail.com>
2022-05-21 15:28:54 +0200

    refact: typing directory-item component

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {FC} from "react";
 import {BackgroundImage, DirectoryItemContainer, ItemBody} from "./directory-item.styles";
 import {useNavigate} from "react-router-dom";
+import {DirectoryCategory} from "../directory/directory.component";
 
-const DirectoryItem = ({ category }) => {
+type DirectoryItemProps = {
+    category: DirectoryCategory
+}
+
+const DirectoryItem: FC<DirectoryItemProps> = ({ category }) => {
   const { imageUrl, title, route } = category;
-  
+
   const navigate = useNavigate();
-  
+
   const onNavigate = () => navigate(route);
   return (
     <DirectoryItemContainer onClick={onNavigate}>
