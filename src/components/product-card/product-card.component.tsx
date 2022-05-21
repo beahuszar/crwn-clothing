b6bae225bbd67754b7d43bf9e76d7c2893a6ae90
beahuszar <husz.beata@gmail.com>
2022-05-21 15:31:41 +0200

    refact: typing product-card component

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -3,12 +3,18 @@ import {Footer, Img, Name, Price, ProductCardContainer} from "./product-card.sty
 import {useDispatch, useSelector} from "react-redux";
 import {addItemToCart} from "../../store/cart/cart.action";
 import {selectCartItems} from "../../store/cart/cart.selector";
+import {CategoryItem} from "../../store/categories/category.types";
+import {FC} from "react";
 
-const ProductCard = ({product}) => {
+type ProductCardProps = {
+    product: CategoryItem
+}
+
+const ProductCard: FC<ProductCardProps> = ({product}) => {
   const {name, price, imageUrl} = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
-  
+
   const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
   return (
     <ProductCardContainer>
